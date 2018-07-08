fcfa9297051d4a1fab7d08820b4146c517b7ddb7
RubyG <rubygillr@gmail.com>
2018-07-08 11:34:37 -0400

    updated html id and div

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,10 +9,46 @@ $(document).ready(function () {
     var correctChoices = 0;
     var wrongChoices = 0;
     var currentQuestion = 0;
+    var unanswered = 0;
 
 
     //FUNCTIONS 
     //________________________________________
+
+    function countDownTimer() {
+        $(".chooseAnswer").click(function () {
+            $(this).data('clicked', true);
+        });
+        var i = 30;
+        var interval = setInterval(function () {
+            if (i < 10) {
+                $("#timerSeconds").html("0" + i);
+                $(".chooseAnswer").on('click', function () {
+                    clearInterval(interval)
+                })
+            } else {
+                $("#timerSeconds").html(i);
+                $(".pickAnswer").on("click", function () {
+                    clearInterval(interval);
+                })
+            }
+            if (i === 0) {
+                unanswered++;
+                clearInterval(interval);
+                currentQuestion++;
+                $("#timer").effect("pulsate", {
+                    times: 25
+                }, 1000 * 5);
+                i = 30;
+                postQuestion(currentQuestion);
+            } else {
+                i--;
+            }
+        }, 1000);
+    }
+
+})
+
 
 
 
@@ -24,4 +60,3 @@ $(document).ready(function () {
 
 
 
-})
