5918a92fdf26143dc128fa20b12ed31dc2a70f98
RubyG <rubygillr@gmail.com>
2018-07-07 20:41:51 -0400

    Updated JS structure

---
+++ assets/javascript/app.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+    //VARIABLES 
+    //__________________________________________
+
+    //Create an array to hold questions in Object Array
+    var questions = "";
+
+    //Hold counters 
+    var correctChoices = 0;
+    var wrongChoices = 0;
+    var currentQuestion = 0;
+
+
+    //FUNCTIONS 
+    //________________________________________
+
+
+
+    //PROCESS
+    //___________________________________________
+
+
+
+
+
+
+})
